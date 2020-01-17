efb5932ab4890d4ab82e0590a7e9f9117609fafb
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-17 04:50:29 +0000

    created Board, List and Card classes

---
+++ assets/js/board.js
@@ -0,0 +1,34 @@
+class Card {
+  constructor(text) {
+    this.text = text;
+  }
+}
+
+class List {
+  constructor(title) {
+    this.title = title;
+    this.cards = [];
+  }
+
+  addCard(text) {
+    var card = new Card(text);
+    this.cards.push(card);
+  }
+}
+
+class Board {
+  constructor() {
+    this.lists = [];
+  }
+
+  addList(text) {
+    var list = new List(text);
+    this.lists.push(list);
+  }
+}
+
+board = new Board();
+board.addList('New list');
+board.lists[0].addCard('New card');
+
+console.log(board);
